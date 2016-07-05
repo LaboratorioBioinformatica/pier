9e57aee86ef53cbd3a1d1c161a3ffc954ee29f45
gianluca <gianluca@guinness>
2016-07-04 21:29:20 -0300

    show option when there no parameters

--- src/main/java/lbi/usp/br/caravela/App.java
+++ src/main/java/lbi/usp/br/caravela/App.java
@@ -15,21 +15,29 @@ public class App {
 	private static final Object AGGREGATOR = "aggregator";
 
 	public static void main(String[] args) throws Exception {
-		String app = args[0];
-		
-		if(app.equals(MY_TAXA_CONVERTER)){
-//			MytaxaConverterFile.main(args);
-		} else if(app.equals(MY_TAXA_TO_PIER)){
-			new MyTaxaFile().mytaxaFileToPierDefaultFile(args[1], args[2]);
-		} else if(app.equals(FEATURES_JOIN)){
-			JsonCreator mappingReader = new JsonCreator(args[1]);
-		} else  if(app.equals(AGGREGATOR)) {
-			FileAggregator fileAggregator = new FileAggregator(args[1]);
-			fileAggregator.createJsonFile();
+		try {
+			String app = args[0];
 			
-		}
-		else {
-			System.out.println("app: " +  app + " not found!");
+			if(app.equals(MY_TAXA_CONVERTER)){
+	//			MytaxaConverterFile.main(args);
+			} else if(app.equals(MY_TAXA_TO_PIER)){
+				new MyTaxaFile().mytaxaFileToPierDefaultFile(args[1], args[2]);
+			} else if(app.equals(FEATURES_JOIN)){
+				JsonCreator mappingReader = new JsonCreator(args[1]);
+			} else  if(app.equals(AGGREGATOR)) {
+				FileAggregator fileAggregator = new FileAggregator(args[1]);
+				fileAggregator.createJsonFile();
+				
+			}
+			else {
+				System.out.println("app: " +  app + " not found!");
+			}
+		} catch (ArrayIndexOutOfBoundsException aiobe) {
+			System.out.println("OPTIONS:");
+			System.out.println("aggregator <sample-config-file.json>");
+			System.out.println("mytaxaToPier <mytaxafilepath> <outputfilepath>");
+		} catch (Exception e) {
+			throw e;
 		}
 	}
 }
