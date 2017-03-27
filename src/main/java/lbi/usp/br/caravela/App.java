3e89546e7c99fec3ff0c1dac07c293e8273b660c
gianluca <gianluca@guinness>
2017-03-27 16:29:22 -0300

    split aggregator and aggregatorByFileName

--- src/main/java/lbi/usp/br/caravela/App.java
+++ src/main/java/lbi/usp/br/caravela/App.java
@@ -9,53 +9,50 @@ import lbi.usp.br.caravela.mytaxa.MyTaxaFile;
  *
  */
 public class App {
-	
-	
+
 	private static final String MY_TAXA_CONVERTER = "mytaxa";
 	private static final String MY_TAXA_TO_PIER = "mytaxaToPier";
 	private static final String CLARK_TO_PIER = "clarkToPier";
 	private static final String FEATURES_JOIN = "join";
 	private static final String AGGREGATOR = "aggregator";
+	private static final String AGGREGATOR_BY_FILE_NAME = "aggregatorByFileName";
 	private static final String CLEAR_PAIR_INFO_FROM_READ_ID = "clearPairInfoFromReadId";
 
 	public static void main(String[] args) throws Exception {
 		try {
 			String app = args[0];
-			
-			if(app.equals(MY_TAXA_CONVERTER)){
-	//			MytaxaConverterFile.main(args);
-			} else if(app.equals(MY_TAXA_TO_PIER)){
+
+			if (app.equals(MY_TAXA_CONVERTER)) {
+				// MytaxaConverterFile.main(args);
+			} else if (app.equals(MY_TAXA_TO_PIER)) {
 				new MyTaxaFile().mytaxaFileToPierDefaultFile(args[1], args[2]);
-			} else if(app.equals(FEATURES_JOIN)){
+			} else if (app.equals(FEATURES_JOIN)) {
 				JsonCreator mappingReader = new JsonCreator(args[1]);
-			} else  if(app.equals(AGGREGATOR)) {
+			} else if (app.equals(AGGREGATOR)) {
 				FileAggregator fileAggregator = new FileAggregator();
-				String sampleDirectoryPath = args[2];
-				if(sampleDirectoryPath.isEmpty()){
-					fileAggregator.createJsonFileByConfigFileInput(args[1]);
-				} else {
-					fileAggregator.createJsonFileByConfigFileDirectoryPath(args[1], sampleDirectoryPath);
-				}
-			}
-			else if(app.equals(CLARK_TO_PIER)){
+				fileAggregator.createJsonFileByConfigFileInput(args[1]);
+			} else if (app.equals(AGGREGATOR_BY_FILE_NAME)) {
+				FileAggregator fileAggregator = new FileAggregator();
+				fileAggregator.createJsonFileByConfigFileDirectoryPath(args[1], args[2]);
+			} else if (app.equals(CLARK_TO_PIER)) {
 				ClackFile clackFile = new ClackFile();
 				clackFile.toPierFile(args[1]);
-			} else if(app.equals(CLEAR_PAIR_INFO_FROM_READ_ID)){
+			} else if (app.equals(CLEAR_PAIR_INFO_FROM_READ_ID)) {
 				ReadIDCleaner readIDCleaner = new ReadIDCleaner();
 				readIDCleaner.removePairInformationOnReadId(args[1]);
-			} 
-			
+			}
+
 			else {
-				System.out.println("app: " +  app + " not found!");
+				System.out.println("app: " + app + " not found!");
 			}
 		} catch (ArrayIndexOutOfBoundsException aiobe) {
 			System.out.println("OPTIONS:");
 			System.out.println("aggregator <sample-config-file.json>");
-			System.out.println("aggregator <sample-name> <directory-path>");
+			System.out.println("aggregatorByFileName <sample-name> <directory-path>");
 			System.out.println("mytaxaToPier <mytaxafilepath> <outputfilepath>");
 			System.out.println("clarkToPier <clarkfilepath>");
 			System.out.println("clearPairInfoFromReadId <taxonomy-file-path>");
-			
+
 		} catch (Exception e) {
 			throw e;
 		}
